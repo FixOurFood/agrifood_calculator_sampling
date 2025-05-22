744634d095afd042c5f01cd95e15413faf6612ea
Sarah Bridle <github@sarahbridle.net>
2025-05-22 21:01:08 +0100

    Added functions to pipeline_setup.py
    Pasted in a load of the stuff from parameter_exploration.ipynb into here so the notebook is a bit shorter

--- pipeline_setup.py
+++ pipeline_setup.py
@@ -2,6 +2,139 @@ from agrifoodpy.pipeline import Pipeline
 from datablock_setup import *
 from model import *
 
+#from datablock_setup import datablock_setup
+from pipeline_setup import pipeline_setup
+import matplotlib.pyplot as plt
+import xarray as xr
+import copy
+import numpy as np
+import pandas as pd
+
+def read_advanced_settings(advanced_settings_url):
+    """Reads the advanced settings from the spreadsheet URL"""
+    advanced_settings  = pd.read_csv(advanced_settings_url, dtype='string')
+    advanced_settings_dict = {}
+
+    for index, row in advanced_settings.iterrows():
+        if row["type"] == "float": 
+            advanced_settings_dict[row["key"]] = float(row["value"])
+        elif row["type"] == "string":
+            advanced_settings_dict[row["key"]] = str(row["value"])
+        elif row["type"] == "bool":
+            advanced_settings_dict[row["key"]] = row["value"] == "TRUE"
+    
+    return advanced_settings_dict
+
+def set_sector_emissions_dict():
+
+    sector_emissions_dict = {
+        "F-gases":1.60,
+        "Waste":5.11,
+        "Shipping":0.51,
+        "Aviation":22.67,
+        "LU sources":20.82, # Sum of 2025 land use sources
+        "Agriculture":0, # Set to be calculated by the model
+        "Fuel supply":1.57, 
+        "Electricity":1.00,
+        "Manufacturing":3.57,
+        "Buildings":1.11, # Sum of residential and non-residential
+        "Transport":1.07,
+        "LU sinks":0, # Set to be calculated by the model
+        "Removals":0, # Set to be calculated by the model
+    }
+    return sector_emissions_dict
+
+# Set the pipeline
+def SSR_emissions(input_datablock, params, sector_emissions_dict):
+
+    datablock_copy = copy.deepcopy(input_datablock)
+    food_system = Pipeline(datablock_copy)
+
+    # SSR
+    food_system = pipeline_setup(food_system, params)
+    food_system.run()
+    datablock_result = food_system.datablock
+
+    metric_yr = 2050
+    gcapday = datablock_result["food"]["g/cap/day"].sel(Year=metric_yr).fillna(0)
+    SSR_metric_yr = gcapday.fbs.SSR()
+
+
+    # Emissions
+    seq_da = datablock_result["impact"]["co2e_sequestration"].sel(Year=metric_yr)
+    emissions = datablock_result["impact"]["g_co2e/year"]["production"].sel(Year=metric_yr)/1e6
+    total_emissions = emissions.sum(dim="Item").values/1e6
+    total_seq = seq_da.sel(Item=["Broadleaf woodland",
+                                    "Coniferous woodland",
+                                    "Managed pasture",
+                                    "Managed arable",
+                                    "Mixed farming",
+                                    "Silvopasture",
+                                    "Agroforestry"]).sum(dim="Item").values/1e6
+
+    total_removals = seq_da.sel(Item=["BECCS from waste", "BECCS from overseas biomass", "BECCS from land", "DACCS"]).sum(dim="Item").values/1e6
+
+    emissions_balance = xr.DataArray(data = list(sector_emissions_dict.values()),
+                            name="Sectoral emissions",
+                            coords={"Sector": list(sector_emissions_dict.keys())})
+
+    emissions_balance.loc[{"Sector": "Agriculture"}] = total_emissions
+    emissions_balance.loc[{"Sector": "LU sinks"}] = -total_seq
+    emissions_balance.loc[{"Sector": "Removals"}] = -total_removals
+
+    emissions_balance.loc[{"Sector": "LU sources"}] -= seq_da.sel(Item=["Restored upland peat", "Restored lowland peat"]).sum(dim="Item").values/1e6
+
+    total_emissions = emissions_balance.sum()
+
+    return SSR_metric_yr.to_numpy(), total_emissions.to_numpy()
+
+
+# Set the scenario parameters - ideally switch to using spreadsheet instead of this
+def set_baseline_scenario(params):
+
+    params["yield_proj"] = 0
+    params["elasticity"] = 0
+    params["ruminant"] = 0
+    params["pig_poultry"] = 0
+    params["fish_seafood"] = 0
+    params["dairy"] = 0
+    params["eggs"] = 0
+    params["fruit_veg"] = 0
+    params["pulses"] = 0
+    params["meat_alternatives"] = 0
+    params["dairy_alternatives"] = 0
+    params["waste"] = 0
+    params["foresting_pasture"] = 13.17
+    params["land_BECCS"] = 0
+    params["horticulture"] = 0
+    params["pulse_production"] = 0
+    params["lowland_peatland"] = 0
+    params["upland_peatland"] = 0
+    params["pasture_soil_carbon"] = 0
+    params["arable_soil_carbon"] = 0
+    params["mixed_farming"] = 0
+    params["silvopasture"] = 0
+    params["nitrogen"] = 0
+    params["methane_inhibitor"] = 0
+    params["stock_density"] = 0
+    params["manure_management"] = 0
+    params["animal_breeding"] = 0
+    params["fossil_livestock"] = 0
+    params["agroforestry"] = 0
+    params["vertical_farming"] = 0
+    params["fossil_arable"] = 0
+    params["waste_BECCS"] = 0
+    params["overseas_BECCS"] = 0
+    params["DACCS"] = 0
+    params["bdleaf_conif_ratio"] = 75
+
+    # Set some additional parameters that aren't in the spreadsheet 
+    params["scaling_nutrient"] = "kCal/cap/day"
+    params["cereal_scaling"] = True
+
+    return params
+
+
 def pipeline_setup(food_system, params):
 
     # Global parameters
