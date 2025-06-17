714a850cfa2c780a19c3da21cdf39bcffa52f05c
jucordero <obejaamarilla@hotmail.com>
2025-06-17 22:23:57 +0100

    Model updates 170625

--- pipeline_setup.py
+++ pipeline_setup.py
@@ -45,13 +45,13 @@ def set_sector_emissions_dict():
     return sector_emissions_dict
 
 # Set the pipeline
-def SSR_emissions(input_datablock, params, sector_emissions_dict):
+def SSR_emissions(input_datablock, params, timing=False):
 
     datablock_copy = copy.deepcopy(input_datablock)
     food_system = Pipeline(datablock_copy)
 
     food_system = pipeline_setup(food_system, params)
-    food_system.run()
+    food_system.run(timing=timing)
     datablock_result = food_system.datablock
 
     SSR_metric_yr = datablock_result["metrics"]["g/cap/daySSR_metric_yr"]
@@ -64,7 +64,7 @@ def SSR_emissions(input_datablock, params, sector_emissions_dict):
 def set_baseline_scenario(params):
 
     params["yield_proj"] = 0
-    params["elasticity"] = 0
+    params["elasticity"] = 0.5
     params["ruminant"] = 0
     params["pig_poultry"] = 0
     params["fish_seafood"] = 0
@@ -77,6 +77,7 @@ def set_baseline_scenario(params):
     params["waste"] = 0
     params["foresting_pasture"] = 13.17
     params["land_BECCS"] = 0
+    params["land_BECCS_pasture"] = 0
     params["horticulture"] = 0
     params["pulse_production"] = 0
     params["lowland_peatland"] = 0
@@ -97,7 +98,9 @@ def set_baseline_scenario(params):
     params["waste_BECCS"] = 0
     params["overseas_BECCS"] = 0
     params["DACCS"] = 0
+    params["biochar"] = 0
     params["bdleaf_conif_ratio"] = 75
+    params["livestock_yield"] = 100
 
     # Set some additional parameters that aren't in the spreadsheet 
     params["scaling_nutrient"] = "kCal/cap/day"
@@ -137,26 +140,48 @@ def pipeline_setup(food_system, params):
                           "scaling_nutrient":params["scaling_nutrient"],
                           "constant":params["cereal_scaling"],
                           "non_sel_items":("Item_group", "Cereals - Excluding Beer")})
- 
-    food_system.add_node(cultured_meat_model,
-                            {"cultured_scale":params["meat_alternatives"]/100,
-                            "labmeat_co2e":params["labmeat_co2e"],
-                            "items":[2731, 2732, 2733, 2734],
-                            "copy_from":2731,
-                            "new_items":5000,
-                            "new_item_name":"Alternative meat",
-                            "source":["production", "imports"],
-                            "elasticity":[params["elasticity"], 1-params["elasticity"]]})
+    
+    food_system.add_node(alternative_food_model,
+                         {"cultured_scale":params["meat_alternatives"]/100,
+                         "labmeat_co2e":params["labmeat_co2e"],
+                         "baseline_items":[2731, 2732, 2733, 2734],
+                         "replaced_items":("Item_group", "Cereals - Excluding Beer"),
+                         "copy_from":2731,
+                         "new_items":5000,
+                         "new_item_name":"Alternative meat",
+                         "source":["production", "imports"],
+                         "elasticity":[params["elasticity"], 1-params["elasticity"]]})
 
-    food_system.add_node(cultured_meat_model,
+    food_system.add_node(alternative_food_model,
                             {"cultured_scale":params["dairy_alternatives"]/100,
                             "labmeat_co2e":params["dairy_alternatives_co2e"],
-                            "items":[2948, 2743, 2740],
+                            "baseline_items":[2948, 2743, 2740],
+                            "replaced_items":("Item_group", "Cereals - Excluding Beer"),
                             "copy_from":2948,
                             "new_items":5001,
                             "new_item_name":"Alternative dairy",
                             "source":["production", "imports"],
                             "elasticity":[params["elasticity"], 1-params["elasticity"]]})
+ 
+    # food_system.add_node(cultured_meat_model,
+    #                         {"cultured_scale":params["meat_alternatives/100,
+    #                         "labmeat_co2e":params["labmeat_co2e,
+    #                         "items":[2731, 2732, 2733, 2734],
+    #                         "copy_from":2731,
+    #                         "new_items":5000,
+    #                         "new_item_name":"Alternative meat",
+    #                         "source":["production", "imports"],
+    #                         "elasticity":[params["elasticity, 1-params["elasticity]})
+
+    # food_system.add_node(cultured_meat_model,
+    #                         {"cultured_scale":params["dairy_alternatives/100,
+    #                         "labmeat_co2e":params["dairy_alternatives_co2e,
+    #                         "items":[2948, 2743, 2740],
+    #                         "copy_from":2948,
+    #                         "new_items":5001,
+    #                         "new_item_name":"Alternative dairy",
+    #                         "source":["production", "imports"],
+    #                         "elasticity":[params["elasticity, 1-params["elasticity]})
     
     if not params["cereal_scaling"]:
         food_system.add_node(item_scaling,
@@ -173,6 +198,9 @@ def pipeline_setup(food_system, params):
                             "source":["production", "imports"],
                             "elasticity":[params["elasticity"], 1-params["elasticity"]]})
 
+    food_system.add_node(production_land_scale,
+                         {"bdleaf_conif_ratio":params["bdleaf_conif_ratio"]/100,}
+                        )
 
     # Land management
     food_system.add_node(forest_land_model_new,
@@ -181,23 +209,49 @@ def pipeline_setup(food_system, params):
                             })
 
     food_system.add_node(BECCS_farm_land,
-                        {"farm_percentage":params["land_BECCS"]/100,
+                        {"land_type": "Arable",
+                         "farm_percentage":params["land_BECCS"]/100,
+                         "items":("Item_origin", "Vegetal Products"),
+                        })
+    
+    food_system.add_node(BECCS_farm_land,
+                        {"land_type": ["Improved grassland", "Semi-natural grassland"],
+                         "farm_percentage":params["land_BECCS_pasture"]/100,
+                         "items":("Item_origin", "Animal Products"),
                         })
 
     food_system.add_node(shift_production,
                          {"scale":params["horticulture"]/100,
-                          "items":("Item_group", ["Vegetables",
-                                                  "Fruits - Excluding Wine",
-                                                  "Vegetables Oils",
-                                                  "Spices",
-                                                  "Starchy Roots",
-                                                  "Sugar Crops",
-                                                  "Oilcrops",
-                                                  "Treenuts",
-                                                  ]),
-                          "items_target":("Item_group", ["Cereals - Excluding Beer",
-                                                         "Pulses",
-                                                         ]),
+                          
+                          "items":[2617, 2775, 2615, 2532, 2614, 2560,
+                                   2619, 2625, 2613, 2620, 2612, 2551,
+                                   2563, 2602, 2611, 2640, 2641, 2618,
+                                   2616, 2531, 2534, 2533, 2601, 2605,
+                                   2535],
+
+                          "items_target":[2659, 2513, 2546, 2656, 2658, 2657,
+                                          2520, 2642, 2633, 2578, 2630, 2559,
+                                          2575, 2572, 2745, 2514, 2582, 2552,
+                                          2517, 2516, 2586, 2570, 2580, 2562,
+                                          2577, 2576, 2547, 2549, 2574, 2558,
+                                          2581, 2515, 2561, 2579, 2518, 2807, 
+                                          2571, 2555, 2645, 2542, 2537, 2536,
+                                          2541, 2557, 2573, 2543, 2635, 2511,
+                                          2655],
+                        #   "items":("Item_group", ["Vegetables",
+                        #                           "Fruits - Excluding Wine",
+                        #                           "Vegetables Oils",
+                        #                           "Spices",
+                        #                           "Starchy Roots",
+                        #                           "Sugar Crops",
+                        #                           "Oilcrops",
+                        #                           "Treenuts",
+                        #                           ]),
+                        
+                          # "items_target":("Item_group", ["Cereals - Excluding Beer",
+                          #                                "Pulses",
+                          #                                ]),
+
                           "land_area_ratio":0.08650301817
                           })
     
@@ -274,8 +328,7 @@ def pipeline_setup(food_system, params):
     
     food_system.add_node(scale_production,
                             {"scale_factor":1+params["stock_density"]/100,
-                             "items":[2731, 2732, 2733, 2735, 2948, 2740, 2743],
-                             "elasticity":[params["elasticity"], 1-params["elasticity"]]})
+                             "items":[2731, 2732, 2733, 2735, 2948, 2740, 2743]})
 
     # food_system.add_node(scale_production,
     #                         {"scale_factor":1-params["methane_prod_factor*params["methane_inhibitor/100,
@@ -304,6 +357,15 @@ def pipeline_setup(food_system, params):
     # food_system.add_node(scale_production,
     #                         {"scale_factor":1 - params["fossil_livestock_prod_factor*params["fossil_livestock/100,
     #                         "items":("Item_origin","Animal Products")})
+
+    food_system.add_node(scale_impact,
+                            {"items":("Item_origin","Animal Products"),
+                            "scale_factor":1-1/(params["livestock_yield"]/100)})
+    
+    food_system.add_node(scale_production,
+                            {"scale_factor":params["livestock_yield"]/100,
+                            "items":("Item_origin","Animal Products")})
+
 
     # Arable farming practices
     food_system.add_node(agroecology_model,
@@ -342,13 +404,17 @@ def pipeline_setup(food_system, params):
     food_system.add_node(ccs_model,
                             {"waste_BECCS":params["waste_BECCS"]*1e6,
                             "overseas_BECCS":params["overseas_BECCS"]*1e6,
-                            "DACCS":params["DACCS"]*1e6})
+                            "DACCS":params["DACCS"]*1e6,
+                            "biochar":params["biochar"]*1e6})
 
+    food_system.add_node(label_new_forest)
 
     # Compute emissions and sequestration
     food_system.add_node(forest_sequestration_model,
                             {"land_type":["Broadleaf woodland",
                                           "Coniferous woodland",
+                                          "New Broadleaf woodland",
+                                          "New Coniferous woodland",
                                           "Restored upland peat",
                                           "Restored lowland peat",
                                           "Managed arable",
@@ -357,13 +423,14 @@ def pipeline_setup(food_system, params):
                                           ],
                             "seq":[params["bdleaf_seq_ha_yr"],
                                    params["conif_seq_ha_yr"],
+                                   params["new_bdleaf_seq_ha_yr"],
+                                   params["new_conif_seq_ha_yr"],
                                    params["peatland_seq_ha_yr"],
                                    params["peatland_seq_ha_yr"],
                                    params["managed_arable_seq_ha_yr"],
                                    params["managed_pasture_seq_ha_yr"],
                                    params["mixed_farming_seq_ha_yr"],
                                    ]})
-
     # Compute emissions
     food_system.add_node(compute_emissions)
 
